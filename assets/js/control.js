579fd86f66b6bda15bcefc9c22e1635be0808d32
Nazmul Hossain <nahidnazmul000@gmail.com>
2021-04-02 09:40:46 +0600

    commit

---
+++ assets/js/control.js
@@ -0,0 +1,37 @@
+document.getElementById('signUp').addEventListener('click', function(){
+    document.getElementById('signUp').className="xyz";
+    document.getElementById('logIn').className="abc";
+});
+
+document.getElementById('logIn').addEventListener('click', function(){
+    document.getElementById('logIn').className="new-active";
+    document.getElementById('signUp').className="new-stop";
+});
+
+
+document.getElementById('btm-line1').addEventListener('click', function(){
+   document.getElementById('div-border').className="div-border";
+});
+document.getElementById('btm-line2').addEventListener('click', function(){
+    document.getElementById('div-border2').className="div-border";
+ });
+
+
+ document.getElementById('btm-line1').addEventListener('keyup', function(){
+    document.getElementById('div-border').className="div-border-edit";
+ });
+ document.getElementById('btm-line2').addEventListener('keyup', function(){
+     document.getElementById('div-border2').className="div-border-edit";
+  });
+  document.getElementById('btm-line1').addEventListener('keyup', function(){
+    document.getElementById('msg').innerHTML="";
+ });
+ document.getElementById('btm-line2').addEventListener('keyup', function(){
+    document.getElementById('eye-msg').innerHTML="";
+  });
+  document.getElementById('btm-line1').addEventListener('keyup', function(){
+    document.getElementById('eye').className="i-decoration";
+ });
+      
+
+ 
